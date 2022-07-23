7193ae75bf195add357013eb46a66f39e8f8b659
Arseny.Fomchenko <fomars@gmail.com>
2022-07-23 17:41:38 +0800

    no cloudcube env

--- app/settings.py
+++ app/settings.py
@@ -11,7 +11,7 @@ LOCAL_API = bool(os.getenv('API_URL'))
 API_WORKDIR = os.getenv('API_WORKDIR', '/var/lib/telegram-bot-api')
 
 # s3
-CLOUDCUBE_URL = os.environ['CLOUDCUBE_URL']
+CLOUDCUBE_URL = os.getenv('CLOUDCUBE_URL')
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 
